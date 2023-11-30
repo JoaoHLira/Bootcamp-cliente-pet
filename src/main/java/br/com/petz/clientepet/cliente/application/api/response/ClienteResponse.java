9725c02a8564c7b486c9a9ee4bcdc317b6a4461d
JoaoHLira <lirarosas.jh@gmail.com>
2023-11-30 19:41:40 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/response/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/response/ClienteResponse.java
@@ -1,4 +1,10 @@
 package br.com.petz.clientepet.cliente.application.api.response;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
 }
