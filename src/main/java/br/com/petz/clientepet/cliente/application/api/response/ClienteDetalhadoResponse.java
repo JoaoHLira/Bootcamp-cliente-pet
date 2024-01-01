8e849b6bd257069816ef96621f758389b146de8e
JoaoHLira <lirarosas.jh@gmail.com>
2024-01-01 20:20:08 -0300

    implementa camada repository, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/api/response/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/response/ClienteDetalhadoResponse.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.cliente.application.api.response;
 
+import br.com.petz.clientepet.cliente.domain.Cliente;
 import br.com.petz.clientepet.cliente.domain.Sexo;
 import lombok.Value;
 
@@ -16,4 +17,15 @@ public class ClienteDetalhadoResponse {
     private Sexo sexo;
     private Boolean aceitaTermos;
     private LocalDateTime dataHoraDoCadastro;
+
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.sexo = cliente.getSexo();
+        this.aceitaTermos = cliente.getAceitaTermos();
+        this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+    }
 }
